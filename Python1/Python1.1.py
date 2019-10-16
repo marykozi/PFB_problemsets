154258df7a9025ce94ad2f29e167a0561db7ae42
info <info@info02.cshl.edu>
2019-10-16 11:31:54 -0400

    adds mane animal etc in twi different ways

--- Python1/Python1.1.py
+++ Python1/Python1.1.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 print("My name: Sofia")
+print("My favorite color: Green")
+my_favorite_activity = '' #create an variable
+my_favorite_activity = "Biking" #put a string in the variable
+
+print("My favorite activity:", my_favorite_activity)
+
+my_favorite_animal = '' #create an variable
+my_favorite_animal = "Dog" #put a string in the variable
+
+print("My favorite animal:", my_favorite_animal)
+
 
---
+++ Python1/Python1.3.py
@@ -0,0 +1,12 @@
+#i /usr/bin/env python3
+import sys
+
+my_name = sys.argv[1] #get first ommand line parameter
+my_favorite_color = sys.argv[2]
+my_favorite_activity = sys.argv[3]
+my_favorite_animal = sys.argv[4]
+
+print("My name:", my_name+ '\n')
+print("My favorite color:", my_favorite_color+ '\n')
+print("My favorite animal:", my_favorite_animal+ '\n')
+print("My favorite activity:", my_favorite_activity+ '\n')
